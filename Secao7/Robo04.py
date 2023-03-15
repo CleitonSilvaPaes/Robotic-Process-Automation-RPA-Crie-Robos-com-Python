9803c370d752280287a15e084240a6016f663b3a
CleitonSilvaPaes <cleiton03122000@gmail.com>
2023-03-15 16:38:50 -0300

    Robo04

--- Secao7/Robo04.py
+++ Secao7/Robo04.py
@@ -8,14 +8,14 @@ x, y = 36, 200
 
 p.doubleClick(x, y)
 p.sleep(1)
-p.getActiveWindow().maximize()
+p.getActiveWindow().maximize()  # type:ignore
 p.write('www.udemy.com')
 p.press('enter')
 p.sleep(10)
 
 img_pesq = local+"\\pesquisa.png"
 local_pesq = p.locateOnScreen(img_pesq,  confidence=.7)
-x, y = p.center(local_pesq)
+x, y = p.center(local_pesq)  # type:ignore
 p.moveTo(x, y)
 p.click(x, y)
 p.sleep(3)
@@ -24,6 +24,6 @@ p.press('enter')
 p.sleep(4)
 img_close = local+"\\close.png"
 local_pesq = p.locateOnScreen(img_close,  confidence=.7)
-x, y = p.center(local_pesq)
+x, y = p.center(local_pesq)  # type:ignore
 p.moveTo(x, y)
 p.click(x, y)
