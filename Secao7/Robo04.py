b2b06c8b9b2f91fb7e6cf8ec72ea3c514ac48f4c
CleitonSilvaPaes <cleiton03122000@gmail.com>
2023-03-15 16:37:41 -0300

    Secao7

---
+++ Secao7/Robo04.py
@@ -0,0 +1,29 @@
+import os
+
+import pyautogui as p
+
+local = os.path.dirname(os.path.realpath(__file__))
+
+x, y = 36, 200
+
+p.doubleClick(x, y)
+p.sleep(1)
+p.getActiveWindow().maximize()
+p.write('www.udemy.com')
+p.press('enter')
+p.sleep(10)
+
+img_pesq = local+"\\pesquisa.png"
+local_pesq = p.locateOnScreen(img_pesq,  confidence=.7)
+x, y = p.center(local_pesq)
+p.moveTo(x, y)
+p.click(x, y)
+p.sleep(3)
+p.write('Charles Lima')
+p.press('enter')
+p.sleep(4)
+img_close = local+"\\close.png"
+local_pesq = p.locateOnScreen(img_close,  confidence=.7)
+x, y = p.center(local_pesq)
+p.moveTo(x, y)
+p.click(x, y)
