991f899fb233ddc632234a8ef6d68665cf765c0b
CleitonSilvaPaes <cleiton03122000@gmail.com>
2023-03-15 13:54:05 -0300

    Completo Secao4

---
+++ Secao4/Robo01.py
@@ -0,0 +1,49 @@
+"""
+Baixando as bibliotecas
+
+pyautogui
+pandas
+rpa
+opencv-python
+
+import pyautogui as p
+
+Desse modo importamos o pyautogui
+
+sleep() -> tempo para esperar para excutar uma funcao
+
+position() -> Serve para pegar as possicao do mouse x, y
+
+moveTo() -> Serve para depois que tiver a possicao do mouse para mover para o local designado. # noqa
+
+click() -> dar um click no arquivo
+
+doubleClick() -> Abrir arquivo
+
+hotkey('win', 'r) -> Serve para tecla de atalho
+
+typewrite -> Para escrever
+
+press('enter') Serve para precionar uma tecla
+
+getActiveWindow() -> Pega a janela atual do windows
+getActiveWindow().close() -> Fecha a janela atual
+"""
+
+import pyautogui as p
+
+# x, y = 40, 202
+
+p.hotkey('win', 'r')
+p.sleep(1)
+p.typewrite('notepad')
+p.sleep(1)
+p.press('enter')
+p.sleep(1)
+p.typewrite('Oi!! Eu sou um Bot!')
+p.sleep(1)
+p.getActiveWindow().close()
+p.sleep(1)
+p.press('right')
+p.sleep(0.5)
+p.press('enter')
